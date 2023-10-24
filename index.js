46c2e66ca8941af98551a8cf03de683f8494c021
inthegarden <83335817+sorrawit2546@users.noreply.github.com>
2023-10-24 18:27:24 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
-var countDownDate = new Date("Dec 25, 2023 00:00:00").getTime();
-var x = setInterval(function(){
-    var now = new Date().getTime();
-    var distance = countDownDate - now;
-    
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+document.getElementById('Login').addEventListener('click', function() {
+    window.location.href = 'http://localhost/login/Login.php';
+});
 
-    document.getElementById("days").innerHTML = days;
-    document.getElementById("hours").innerHTML = hours;
-    document.getElementById("minutes").innerHTML = minutes;
-    document.getElementById("seconds").innerHTML = seconds;
-
-},1000);
+document.getElementById('sign-up').addEventListener('click', function() {
+    window.location.href = 'http://localhost/login/signup.php';
+});
